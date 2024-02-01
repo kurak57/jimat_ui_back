ee74fafbbeb172c6fc8ecdc3968245287124b1e2
kurak57 <tomitimotius92@gmail.com>
2024-02-01 09:09:18 +0700

    make new connection DB

--- config/Database.js
+++ config/Database.js
@@ -4,6 +4,7 @@ dotenv.config();
 
 const db = new Sequelize(process.env.DB_DBNAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
     host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     dialect: "mysql"
 });
 
