35e3af0f5467250ab02a23af0859381ce8f24e10
kurak57 <tomitimotius92@gmail.com>
2024-02-03 09:48:53 +0700

    vercel json update

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ app.use(session({
 
 app.use(cors({
     credentials: true,
-    // origin: 'http://localhost:3000'
+    origin: 'http://localhost:3000'
 }));
 app.use(express.json());
 app.use(UserRoute);
