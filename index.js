55bd675bbece2e0b1a955c779d61df55326ce7ff
kurak57 <tomitimotius92@gmail.com>
2024-02-09 10:30:04 +0700

    crdental aagain

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ app.use(session({
 
 app.use(cors({
     credentials: true,
-    origin: 'http://localhost:3000' || 'https://jimat-ui-front.vercel.app'
+    origin: 'https://jimat-ui-front.vercel.app'
 }));
 app.use(express.json());
 app.use(UserRoute);
