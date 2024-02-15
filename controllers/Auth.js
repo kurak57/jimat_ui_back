d18b0330c9db47c1aa3e4a79ff64d332796ea3f3
kurak57 <tomitimotius92@gmail.com>
2024-02-15 15:48:58 +0700

    add jwt

--- controllers/Auth.js
+++ controllers/Auth.js
@@ -3,6 +3,7 @@ import sendEmail from "../utils/sendEmail.js";
 import crypto from "crypto";
 import User from "../models/UserModel.js";
 import argon2 from 'argon2';
+import jwt from 'jsonwebtoken';
 
 export const Login = async (req, res) => {
     try {
@@ -15,25 +16,40 @@ export const Login = async (req, res) => {
         const match = await argon2.verify(user.password, req.body.password);
         if(!match) return res.status(400).json({msg: "Password Salah"});
         req.session.userId = user.uuid;
-
         if(!user.isVerified){
-            let token = await Token.findOne({ where: { userId: user.id }, });
-            if (!token) {
-                token = await Token.create({
+            let emailToken = await Token.findOne({ where: { userId: user.id }, });
+            if (!emailToken) {
+                emailToken = await Token.create({
                     userId: user.uuid,
                     token: crypto.randomBytes(32).toString("hex") 
                  })
-                const url = `${process.env.Base_url}/users/${user.uuid}/verify/${token.token}`;
+                const url = `${process.env.Base_url}/users/${user.uuid}/verify/${emailToken.token}`;
                 await sendEmail(user.email, "Verify Email", url);
             } 
             return res.status(400).send({ msg: "An Email sent to your account please verify" });
         }
-        const uuid = user.uuid;
-        const name = user.name;
-        const email = user.email;
-        const role = user.role;
-        const verified = user.isVerified;
-        return res.status(200).json({uuid, name, email, role});
+       
+        const secret = process.env.JWT_SECRET;
+        const expiresIn = 60 * 60 * 2
+        const payload = {
+            uuid: user.uuid, 
+            name: user.name, 
+            email: user.email, 
+            role: user.role
+        }
+        const token = jwt.sign(payload, secret, {expiresIn: expiresIn})
+
+        return res.status(200).json({
+            data: {
+                uuid: user.uuid, 
+                name: user.name, 
+                email: user.email, 
+                role: user.role
+            },
+            token: token
+        }
+            
+        );
     } catch (error) {
         res.status(500).send({ msg: "Internal Server Error" });
     }
@@ -42,6 +58,9 @@ export const Login = async (req, res) => {
 
 export const Me  = async (req, res) => {
     try {
+        if(!req.session.userId){
+            return res.status(401).json({msg: "Mohon login ke akun anda!"})
+        };
         const user = await User.findOne({
             attributes: ['uuid', 'fakultas', 'name', 'email', 'role'],
             where: {
