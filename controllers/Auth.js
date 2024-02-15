db1ec63eab6037835d1152a3b131c1dd7f3aeb47
kurak57 <tomitimotius92@gmail.com>
2024-02-15 15:58:42 +0700

    fix session

--- controllers/Auth.js
+++ controllers/Auth.js
@@ -16,6 +16,25 @@ export const Login = async (req, res) => {
         const match = await argon2.verify(user.password, req.body.password);
         if(!match) return res.status(400).json({msg: "Password Salah"});
         req.session.userId = user.uuid;
+
+        if(!user.isVerified){
+            let token = await Token.findOne({ where: { userId: user.id }, });
+            if (!token) {
+                token = await Token.create({
+                    userId: user.uuid,
+                    token: crypto.randomBytes(32).toString("hex") 
+                 })
+                const url = `${process.env.Base_url}/users/${user.uuid}/verify/${token.token}`;
+                await sendEmail(user.email, "Verify Email", url);
+            } 
+            return res.status(400).send({ msg: "An Email sent to your account please verify" });
+        }
+        const uuid = user.uuid;
+        const name = user.name;
+        const email = user.email;
+        const role = user.role;
+        return res.status(200).json({uuid, name, email, role});
+
     } catch (error) {
         res.status(500).send({ msg: "Internal Server Error" });
     }
