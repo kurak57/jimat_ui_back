f305a6d668d03856d8e19e47f35d3e073b289c47
kurak57 <tomitimotius92@gmail.com>
2024-02-15 15:55:10 +0700

    aneh2

--- controllers/Auth.js
+++ controllers/Auth.js
@@ -16,28 +16,6 @@ export const Login = async (req, res) => {
         const match = await argon2.verify(user.password, req.body.password);
         if(!match) return res.status(400).json({msg: "Password Salah"});
         req.session.userId = user.uuid;
-        if(!user.isVerified){
-            let emailToken = await Token.findOne({ where: { userId: user.id }, });
-            if (!emailToken) {
-                emailToken = await Token.create({
-                    userId: user.uuid,
-                    token: crypto.randomBytes(32).toString("hex") 
-                 })
-                const url = `${process.env.Base_url}/users/${user.uuid}/verify/${emailToken.token}`;
-                await sendEmail(user.email, "Verify Email", url);
-            } 
-            return res.status(400).send({ msg: "An Email sent to your account please verify" });
-        }
-       
-        const secret = process.env.JWT_SECRET;
-        const expiresIn = 60 * 60 * 2
-        const payload = {
-            uuid: user.uuid, 
-            name: user.name, 
-            email: user.email, 
-            role: user.role
-        }
-        const token = jwt.sign(payload, secret, {expiresIn: expiresIn})
 
         return res.status(200).json({
             data: {
